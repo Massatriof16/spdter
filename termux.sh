b3e36b874a2669d03bcd38bb806547fd40af17fe
Massatrio16 <dimassetosatrio@gmail.com>
2025-07-22 15:51:38 +0700

    tes

--- termux.sh
+++ termux.sh
@@ -1,12 +1,12 @@
 
 chmod a+x *
 termux-usb -l > file.txt
-
-
 # Ambil path USB dari file.txt dengan grep dan sed
 USB_DEVICE=$(grep -o '/dev/bus/usb/[0-9]*/[0-9]*' file.txt)
 
 # Jalankan perintah termux-usb dengan perangkat yang ditemukan
+
+
 termux-usb -r "$USB_DEVICE"
 
 termux-usb -e './spd_dump --usb-fd' "$USB_DEVICE"
